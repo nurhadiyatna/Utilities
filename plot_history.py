06f7d3d8cbe69c097561216c96db69f93fc5d9bf
nurhadiyatna <adinurhadiyatna@gmail.com>
2018-01-18 16:19:26 +0100

    plot history

---
+++ plot_history.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy
+
+dataset = numpy.loadtxt("training2.log", delimiter=",")
+
+#dataset = open('training2.log','r').readlines()
+#w =  dataset[0].split()
+#print w
